653fc2824e9acb5e29f5d6dd87da001737a2638e
Furkan <fkaragoz14@gmail.com>
2022-04-20 14:14:32 +0300

    Initial commit of the project.

---
+++ F746_Btn_Int/Core/Inc/main.h
@@ -0,0 +1,37 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f746xx.h"
+#include "RccConfig.h"
+#include "Delay.h"
+
+
+/**
+ * @brief Configures the GPIO.
+ *
+ */
+void GPIO_Config (void);
+
+/**
+ * @brief Configures the interrupt.
+ *
+ */
+void Interrupt_Config (void);
+
+/**
+ * @brief Interrupt handler for EXTI 15:10
+ *
+ */
+void EXTI15_10_IRQHandler(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
